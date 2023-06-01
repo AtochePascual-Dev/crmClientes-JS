883ba8cf278330497e514a6c658b17424f4bcbbf
Atoche Pascual <jmatochepascual@gmail.com>
2023-06-01 07:01:20 -1000

    [ADD] listarCliente

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 (function () {
-
+  const listaClienteHtml = document.querySelector('#listado-clientes');
   let DB;
 
   // Cuando el documento esta listo
@@ -36,7 +36,47 @@
     // Cuando la base de datos se crea correctamente
     baseDatos.onsuccess = () => {
       DB = baseDatos.result;
+      listarCliente();
     };
+  };
+
+
+
+  // * Lista los clientes de la BD 
+  const listarCliente = () => {
+    const transaction = DB.transaction('crm');
+    const objectStore = transaction.objectStore('crm');
+
+    objectStore.openCursor().onsuccess = (event) => {
+
+      // obtenemos un registro de la bd
+      const cursor = event.target.result;
+
+      if (cursor) {
+        const { nombre, correo, telefono, empresa, id } = cursor.value;
+        const tr = document.createElement('tr');
+
+        tr.innerHTML = ` 
+          <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
+              <p class="text-sm leading-10 text-gray-700"> ${correo} </p>
+          </td>
+          <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+              <p class="text-gray-700">${telefono}</p>
+          </td>
+          <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+              <p class="text-gray-600">${empresa}</p>
+          </td>
+          <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+              <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+              <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+          </td>
+  `;
+        listaClienteHtml.appendChild(tr);
+        // Ve al siguiente elemento
+        cursor.continue();
+      };
+    }
   };
 
 })();

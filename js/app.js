8eea2855cf501aae640a5f8bc703a95f2282b5c3
Atoche Pascual <jmatochepascual@gmail.com>
2023-06-01 11:06:55 -1000

    [ADD] eliminarCliente

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@
   // Cuando el documento esta listo
   document.addEventListener("DOMContentLoaded", () => {
     crearBD();
+    listaClienteHtml.addEventListener('click', eliminarCliente);
   });
 
 
@@ -69,12 +70,35 @@
           </td>
           <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
               <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-              <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+              <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
           </td>
   `;
         listaClienteHtml.appendChild(tr);
         // Ve al siguiente elemento
         cursor.continue();
+      };
+    }
+  };
+
+
+  // * Elimina un cliente de bd 
+  const eliminarCliente = (event) => {
+    if (event.target.classList.contains('eliminar')) {
+      const idEliminar = Number(event.target.dataset.cliente);
+
+      const transaction = DB.transaction(['crm'], 'readwrite');
+      const objectStore = transaction.objectStore('crm');
+
+      objectStore.delete(idEliminar);
+
+      // si hay un error
+      transaction.onerror = () => {
+        console.log('hubo un error');
+      };
+
+      // cuando todo sale bien
+      transaction.oncomplete = () => {
+        event.target.parentElement.parentElement.remove();
       };
     }
   };

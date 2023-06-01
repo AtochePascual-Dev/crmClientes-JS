9590ba3f3dbd1a726c19acf82e4af3cd8f7528d4
Atoche Pascual <jmatochepascual@gmail.com>
2023-06-01 04:58:41 -1000

    [ADD] imprimirAlerta

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -3,13 +3,15 @@
   let DB;
 
 
-  // cundo el documento esta listo
+  //* cundo el documento esta listo
   document.addEventListener('DOMContentLoaded', () => {
     conectarDB();
     formulario.addEventListener('submit', agregarCliente);
   });
 
-  // Conecta con la base de datos, si no bd la crea si hay la conecta
+
+
+  //* Conecta con la base de datos, si no bd la crea si hay la conecta
   function conectarDB() {
     const abrirConexion = window.indexedDB.open('crm', 1);
 
@@ -22,7 +24,9 @@
     };
   }
 
-  // valida un cliente
+
+
+  //* valida un cliente
   const agregarCliente = (event) => {
     event.preventDefault();
 
@@ -34,8 +38,33 @@
 
     // Valdamos si existen vacios
     if ([nombre, correo, telefono, empresa].includes('')) {
-      console.log('error');
+      imprimirAlerta('Todos los campos son obligatorios', false);
       return;
     }
+  };
+
+
+
+  //* Imprime una alerta en pantalla
+  const imprimirAlerta = (mensaje, exito = true) => {
+    const existeAlerta = document.querySelector('.alerta');
+
+    if (existeAlerta) return;
+
+    const divAlerta = document.createElement('div');
+    divAlerta.textContent = mensaje;
+    divAlerta.classList.add('px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'text-center', 'border', 'mt-6', 'alerta');
+
+    (exito)
+      ? divAlerta.classList.add('bg-green-100', 'border-green-400', 'text-green-700')
+      : divAlerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700');
+
+    // Mostramos la lerta en pantalla
+    formulario.appendChild(divAlerta);
+
+    // Eliminamos la alerta
+    setTimeout(() => {
+      divAlerta.remove();
+    }, 3000);
   };
 })();
